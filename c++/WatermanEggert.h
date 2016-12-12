a12255c250d017d286ca359ddd552570314e49a5
Roman Cheplyaka <roma@ro-che.info>
2016-12-12 11:20:56 +0200

    Fix calculations

--- c++/WatermanEggert.h
+++ c++/WatermanEggert.h
@@ -21,24 +21,34 @@ template<typename V> double similarity(const Scoring &scoring, const V &a, const
   std::vector<double> matrix(m * n); // row-major
 
   // fill upper-left corner
-  matrix[0] = a[0] == b[0] ? scoring.match_value : 0.0; // assuming mismatch_value <= 0 and match_value >= 0
+  matrix[0] = std::max(
+    a[0] == b[0] ? scoring.match_value : scoring.mismatch_value,
+    0.0
+  );
 
   // fill upper row
   for (int j = 1; j < n; j++) {
-    matrix[j] = std::max(matrix[j-1] + scoring.space_value, 0.0);
+    matrix[j] = std::max({
+      matrix[j-1] + scoring.space_value,
+      a[0] == b[j] ? scoring.match_value : scoring.mismatch_value,
+      0.0});
   }
   // fill leftmost column
   for (int i = 1; i < m; i++) {
-    matrix[i * n] = std::max(matrix[(i-1) * n] + scoring.space_value, 0.0);
+    matrix[i * n] = std::max({
+      matrix[(i-1) * n] + scoring.space_value,
+      a[i] == b[0] ? scoring.match_value : scoring.mismatch_value,
+      0.0
+    });
   }
   // fill the rest of the matrix
   for (int i = 1; i < m; i++) {
   for (int j = 1; j < n; j++) {
     matrix[i * n + j] = std::max({
       0.0,
-      matrix[(i-1) * n + (j-1)] + (a[i] == b[i] ? scoring.match_value : scoring.mismatch_value),
-      matrix[(i-1) * n + j],
-      matrix[i * n + (j - 1)]
+      matrix[(i-1) * n + (j-1)] + (a[i] == b[j] ? scoring.match_value : scoring.mismatch_value),
+      matrix[(i-1) * n + j] + scoring.space_value,
+      matrix[i * n + (j - 1)] + scoring.space_value
       });
   }}
 
