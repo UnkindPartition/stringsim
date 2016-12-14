cb1113567f82deed35529574a64e9e671c80c4d2
Roman Cheplyaka <roma@ro-che.info>
2016-12-14 14:31:24 +0200

    Use long instead of double

--- c++/WatermanEggert.h
+++ c++/WatermanEggert.h
@@ -5,12 +5,12 @@
 #include <algorithm>
 
 struct Scoring {
-  double match_value,
+  long match_value,
          mismatch_value,
          space_value;
 };
 
-template<typename V> void fill_matrix(Scoring scoring, const V &a, const V &b, std::vector<double> &matrix) {
+template<typename V> void fill_matrix(Scoring scoring, const V &a, const V &b, std::vector<long> &matrix) {
   const size_t m = a.size(), // rows
                n = b.size(); // columns
   if (n == 0 || m == 0) {
@@ -20,7 +20,7 @@ template<typename V> void fill_matrix(Scoring scoring, const V &a, const V &b, s
   // fill upper-left corner
   matrix[0] = std::max(
     a[0] == b[0] ? scoring.match_value : scoring.mismatch_value,
-    0.0
+    0L
   );
 
   // fill upper row
@@ -28,21 +28,21 @@ template<typename V> void fill_matrix(Scoring scoring, const V &a, const V &b, s
     matrix[j] = std::max({
       matrix[j-1] + scoring.space_value,
       a[0] == b[j] ? scoring.match_value : scoring.mismatch_value,
-      0.0});
+      0L});
   }
   // fill leftmost column
   for (size_t i = 1; i < m; i++) {
     matrix[i * n] = std::max({
       matrix[(i-1) * n] + scoring.space_value,
       a[i] == b[0] ? scoring.match_value : scoring.mismatch_value,
-      0.0
+      0L
     });
   }
   // fill the rest of the matrix
   for (size_t i = 1; i < m; i++) {
   for (size_t j = 1; j < n; j++) {
     matrix[i * n + j] = std::max({
-      0.0,
+      0L,
       matrix[(i-1) * n + (j-1)] + (a[i] == b[j] ? scoring.match_value : scoring.mismatch_value),
       matrix[(i-1) * n + j] + scoring.space_value,
       matrix[i * n + (j - 1)] + scoring.space_value
@@ -52,13 +52,13 @@ template<typename V> void fill_matrix(Scoring scoring, const V &a, const V &b, s
 
 // a_start and a_end are *half-open* ends of the aligned subsequence
 template<typename V> void find_alignment(Scoring scoring, const V &a, const V &b,
-  const std::vector<double> &matrix,
+  const std::vector<long> &matrix,
   size_t &a_begin, size_t &a_end, size_t &b_begin, size_t &b_end) {
 
   const size_t m = a.size(), // rows
                n = b.size(); // columns
 
-  std::vector<double>::const_iterator end = std::max_element(matrix.begin(), matrix.end());
+  std::vector<long>::const_iterator end = std::max_element(matrix.begin(), matrix.end());
 
   size_t
     i = (end - matrix.begin()) / n,
@@ -67,7 +67,7 @@ template<typename V> void find_alignment(Scoring scoring, const V &a, const V &b
   a_begin = a_end = i + 1;
   b_begin = b_end = j + 1;
 
-  double this_value;
+  long this_value;
 
   // traceback
   while ((i > 0 || j > 0) && (this_value = matrix[i*n+j]) > 0) {
@@ -86,7 +86,7 @@ template<typename V> void find_alignment(Scoring scoring, const V &a, const V &b
   }
 }
 
-template<typename V> double similarity(Scoring scoring, const V &a, const V &b) {
+template<typename V> long similarity(Scoring scoring, const V &a, const V &b) {
   const size_t m = a.size(), // rows
                n = b.size(); // columns
 
@@ -94,7 +94,7 @@ template<typename V> double similarity(Scoring scoring, const V &a, const V &b) 
     return 0;
   }
 
-  std::vector<double> matrix(m * n); // row-major
+  std::vector<long> matrix(m * n); // row-major
 
   fill_matrix(scoring, a, b, matrix);
 
