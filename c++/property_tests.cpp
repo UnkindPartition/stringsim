db5679f92a005441e3e29ed837a1f565477dd022
Roman Cheplyaka <roma@ro-che.info>
2016-12-16 11:03:17 +0200

    Finish fill_matrix property test

--- c++/property_tests.cpp
+++ c++/property_tests.cpp
@@ -6,14 +6,31 @@
 #include "test_utils.h"
 
 using namespace std;
+using namespace rc;
 
-inline vector<char> aString() { return *rc::gen::container<std::vector<char>>(rc::gen::inRange('a', 'e')); }
+inline string aString() { return *gen::container<std::string>(gen::inRange('a', 'e')); }
 
 int main() {
-  rc::check("fill_matrix is properly restricted",
+  check("fill_matrix is properly restricted",
     []() {
-      const auto s1 = aString(), s2 = aString();
+      const auto a = aString(), b = aString();
+      const size_t a_begin = *gen::resize(kNominalSize, gen::inRange(0UL, a.size()+1)),
+                   a_end   = *gen::resize(kNominalSize, gen::inRange(a_begin, a.size()+1)),
+                   b_begin = *gen::resize(kNominalSize, gen::inRange(0UL, b.size()+1)),
+                   b_end   = *gen::resize(kNominalSize, gen::inRange(b_begin, b.size()+1));
+      vector<long> matrix = alloc_fill_matrix(a, b, a_begin, a_end, b_begin, b_end);
 
+      bool any = false;
+      for (size_t i = 0; i < a.size(); i++) {
+      for (size_t j = 0; j < b.size(); j++) {
+        RC_ASSERT(
+          (i >= a_begin && i < a_end && j >= b_begin && j < b_end) ||
+          (matrix[i * b.size() + j] == 0)
+        );
+        any = any || matrix[i * b.size() + j];
+      }
+      }
+      RC_TAG(any ? "not all zero" : "all zero");
 
     });
 
